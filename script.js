153562193bfcca31bcc48ef145c77cff7b735b17
tibraj <tibraj94@gmail.com>
2020-12-17 15:24:32 -0500

    function updating values

--- script.js
+++ script.js
@@ -26,9 +26,20 @@ function addTransactionDOM(transaction) {
     list.appendChild(item);
 }
 
+function updateValues() {
+    const amounts = transactions.map(transaction => transaction.amount);
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+    const income = amounts.filter(item => item > 0).reduce((acc,item) => (acc += item), 0).toFixed(2);
+    const expense = (amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) * -1).toFixed(2);
+    balance.innerText = `$${total}`;
+    money_plus.innerText = `$${income}`;
+    money_minus.innerText = `$${expense}`;
+}
+
 function init() {
     list.innerHTML = '';
     transactions.forEach(addTransactionDOM);
+    updateValues();
 }
 
 init();
