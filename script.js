9961e23dfd79f276905760bd9ab91510ea644a82
tibraj <tibraj94@gmail.com>
2020-12-17 15:12:06 -0500

    function adding transaction to history

--- script.js
+++ script.js
@@ -11,4 +11,17 @@ const dummyTransactions = [
     {id: 2, text: 'Salary', amount: 300},
     {id: 3, text: 'Book', amount: -10},
     {id: 4, text: 'Camera', amount: 150}
-]
+];
+
+let transactions = dummyTransactions;
+
+function addTransactionDOM(transaction) {
+    const sign = transaction.amount < 0 ? '-' : '+';
+    const item = document.createElement('li');
+    item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+    item.innerHTML = `
+    ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span>
+    <button class="delete-btn">x</button>
+    `;
+    list.appendChild(item);
+}
